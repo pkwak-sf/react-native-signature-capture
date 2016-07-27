fcf26584c3d35cc62aef6fa458d202242c1af312
pkwak-sf <pkwak@spredfast.com>
2016-07-27 14:30:19 -0500

    remove dotted border from iOS

--- ios/RSSignatureView.m
+++ ios/RSSignatureView.m
@@ -8,7 +8,6 @@
 #define DEGREES_TO_RADIANS(x) (M_PI * (x) / 180.0)
 
 @implementation RSSignatureView {
-	CAShapeLayer *_border;
 	BOOL _loaded;
 	EAGLContext *_context;
 	UIButton *saveButton;
@@ -24,12 +23,6 @@
 - (instancetype)init
 {
 	if ((self = [super init])) {
-		_border = [CAShapeLayer layer];
-		_border.strokeColor = [UIColor blackColor].CGColor;
-		_border.fillColor = nil;
-		_border.lineDashPattern = @[@4, @2];
-		
-		[self.layer addSublayer:_border];
 	}
 	
 	return self;
@@ -64,8 +57,6 @@
         [self addSubview:sign];
 	}
 	_loaded = true;
-	_border.path = [UIBezierPath bezierPathWithRect:self.bounds].CGPath;
-	_border.frame = self.bounds;
 }
 
 - (void)setRotateClockwise:(BOOL)rotateClockwise {
